1196e4be041ca374762bd444216d9870ea60a1d3
okesseko <59650459+okesseko@users.noreply.github.com>
2022-02-22 00:22:33 +0800

    [feat]: add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import Install from "./components/Install";
+import Install from "./pages/Install";
+import About from "./pages/About";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div id="app">{window.ethereum ? <Home /> : <Install />}</div>;
+  console.log(123);
+  return (
+    <div id="app">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/install" element={<Install />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+
+  // <div id="app">{window.ethereum ? <Home /> : <Install />}</div>;
 }
 
 export default App;
