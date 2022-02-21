ae4207cb8188687a35c983d190616b211eeb2240
okesseko <59650459+okesseko@users.noreply.github.com>
2022-02-22 00:23:43 +0800

    [feat]: about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div id="layout">
+      <p className="text-center text-3xl leading-[4rem]">
+        This project is build by hardhat & vite react.
+        <br />
+        Images are deployed on the{" "}
+        <a target="_blank" href="https://www.pinata.cloud/">
+          Pinata
+        </a>{" "}
+        a IFPS file system.
+        <br />
+        Smart contract is deployed on the Polygon test net{" "}
+        <b className="text-green-400">Mumbai</b> by{" "}
+        <a target="_blank" href="https://www.alchemy.com/">
+          Alchemy
+        </a>{" "}
+        .
+        <br />
+        And final, website is deployed on the firebase. <br />
+      </p>
+      <p className="mt-14 text-xl">
+        If you like this project please give me a star on{" "}
+        <a target="_blank" href="https://github.com/okesseko/shape-man">
+          Github
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default About;
