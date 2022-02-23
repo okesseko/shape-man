cd6ee2d63513e9067673e92f070a2090cd1827ac
okesseko <59650459+okesseko@users.noreply.github.com>
2022-02-23 23:58:26 +0800

    [refactor]: add switch network function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,48 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../../img/logo.png";
 
-const Navbar = () => {
+const Navbar = ({ account, setAccount }) => {
+  async function changeNetwork() {
+    try {
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: "0x13881" }],
+      });
+    } catch (switchError) {
+      if (switchError.code === 4902) {
+        try {
+          await window.ethereum.request({
+            method: "wallet_addEthereumChain",
+            params: [
+              {
+                chainId: "0x13881", // 80001 (Mumbai)
+                chainName: "Mumbai",
+                nativeCurrency: {
+                  name: "MATIC",
+                  symbol: "MATIC",
+                  decimals: 18,
+                },
+                rpcUrls: ["https://matic-mumbai.chainstacklabs.com"],
+                blockExplorerUrls: ["https://mumbai.polygonscan.com/"],
+              },
+            ],
+          });
+        } catch (addError) {
+          console.log(addError);
+        }
+      }
+    }
+    const [account] = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    if (account) setAccount(account);
+  }
+
+  function ellipsisAccount(account) {
+    return account.slice(0, 6) + "..." + account.slice(-4);
+  }
+
   return (
     <nav className="fixed flex w-screen justify-between bg-green-400">
       <Link to="/">
@@ -16,8 +56,11 @@ const Navbar = () => {
           <Link to="/install">Install</Link>
         </li>
         <li className="my-3 mx-6 text-xl">
-          <button className="rounded-full bg-black px-4 py-2">
-            Connect Wallet
+          <button
+            className="rounded-full bg-black px-4 py-2"
+            onClick={changeNetwork}
+          >
+            {account ? ellipsisAccount(account) : "Connect Wallet"}
           </button>
         </li>
       </ul>
