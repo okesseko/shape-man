734d285b2b4cdce59398a72bd75fb8dbe88ab110
okesseko <59650459+okesseko@users.noreply.github.com>
2022-02-26 23:56:49 +0800

    [refactor]: improve the contract

--- contracts/ShapeMan.sol
+++ contracts/ShapeMan.sol
@@ -1,16 +1,21 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
 
-contract ShapeMan is ERC721, ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
+contract ShapeMan is ERC721, ERC721Enumerable, Ownable {
 
-    Counters.Counter private _tokenIdCounter;
+    uint256 public constant MAX_SUPPLY = 1000;
+    uint256 public mintPrice = 0.05 ether;
+    uint256 public maxMint = 1;
 
-    mapping(string=>uint8) existURIs;
+    string public baseURI;
+
+    bool public saleIsActive = false;
 
     constructor() ERC721("ShapeMan", "SM") {}
 
@@ -18,47 +23,57 @@ contract ShapeMan is ERC721, ERC721URIStorage, Ownable {
         return "ipfs://";
     }
 
-    function safeMint(address to, string memory uri) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-    }
+    function payToMint(uint256 tokenQuantity) public payable {
+        require(
+            totalSupply() + 1 <= MAX_SUPPLY,
+            "All NTF are minted"
+        );
+        require(msg.value >= mintPrice, "At least need 0.05 ether");
+        require(tokenQuantity <= maxMint, "Can only mint 1 tokens at a time");
 
-    // The following functions are overrides required by Solidity.
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
-    function isNFTOwned(string memory uri) public view returns(bool) {
-        return existURIs[uri] == 1;
-    }
-
-    function payToMint(address recipient,string memory metaDataURI) public payable returns (uint256){
-        require(existURIs[metaDataURI] != 1,"NTF already minted!");
-        require(msg.value >= 0.05 ether, "At least need 0.8 ether");
-
-        uint256 newId =  _tokenIdCounter.current();
-         _tokenIdCounter.increment();
-         existURIs[metaDataURI] = 1;
-
-         _mint(recipient,newId);
-         _setTokenURI(newId,metaDataURI);
-
-         return newId;
+        for (uint256 i = 0; i < tokenQuantity; i++) {
+            uint256 mintIndex = totalSupply();
+            if (totalSupply() < MAX_SUPPLY) {
+                _safeMint(msg.sender, mintIndex);
+            }
+        }
     }
 
     function count() public view returns (uint256){
-        return _tokenIdCounter.current();
+        return totalSupply();
+    }
+
+
+    //for owner 
+    function setMintPrice(uint256 _mintPrice) public onlyOwner {
+        mintPrice = _mintPrice;
+    }
+    function setMaxMint(uint256 _maxMint) public onlyOwner {
+        maxMint = _maxMint;
+    }
+    function setSaleActive() public onlyOwner {
+        saleIsActive = !saleIsActive;
+    }
+    function withdraw(address to) public onlyOwner {
+        uint256 balance = address(this).balance;
+        payable(to).transfer(balance);
+    }
+
+    // override 
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
