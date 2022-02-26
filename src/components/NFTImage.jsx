c91a997bd96bcf5660f1d530e587932f4cb0d044
okesseko <59650459+okesseko@users.noreply.github.com>
2022-02-26 23:58:58 +0800

    [feat]: remove  mint function

--- src/components/NFTImage.jsx
+++ src/components/NFTImage.jsx
@@ -1,43 +1,22 @@
-import React, { useState } from "react";
-import { ethers } from "ethers";
-import loadingImg from "../../img/loading.gif";
-import questionImg from "../../img/question.svg";
+import React from "react";
 
-const NFTImage = ({
-  disable,
-  isMinted,
-  tokenId,
-  contract,
-  signer,
-  successfulCallback,
-}) => {
+const NFTImage = ({ tokenId }) => {
   const imageIFPSId = import.meta.env.VITE_PINATA_ID;
-  const metaDataURI = `${imageIFPSId}/${tokenId}.json`;
   const imageURI = `https://gateway.pinata.cloud/ipfs/${imageIFPSId}/${tokenId}.png`;
 
-  const [loading, setLoading] = useState(false);
+  async function downloadImage() {
+    const image = await fetch(
+      `https://gateway.pinata.cloud/ipfs/${imageIFPSId}/${tokenId}.png`
+    );
+    const imageBlog = await image.blob();
+    const imageURL = URL.createObjectURL(imageBlog);
 
-  async function mintNFT() {
-    try {
-      const connection = contract.connect(signer);
-      const address = connection.address;
-      const result = await contract.payToMint(address, metaDataURI, {
-        value: ethers.utils.parseEther("0.05"),
-      });
-      setLoading(true);
-      await result.wait();
-      successfulCallback();
-    } catch (e) {
-      window.alert(e.message);
-      console.log(e);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  async function getURI() {
-    const uri = await contract.tokenURI(tokenId);
-    window.alert(`uri is:\n ${uri}`);
+    const link = document.createElement("a");
+    link.href = imageURL;
+    link.download = `shape man-${tokenId}`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }
 
   return (
@@ -46,26 +25,17 @@ const NFTImage = ({
         className="rounded-lg bg-white"
         width={250}
         height={250}
-        src={isMinted ? imageURI : loading ? loadingImg : questionImg}
+        alt={`ID: ${tokenId}`}
+        src={imageURI}
       />
       <div>
-        <h5 className="mb-2 text-3xl">{isMinted ? `ID: ${tokenId}` : ""}</h5>
-        {!isMinted ? (
-          <button
-            className="w-full rounded-lg border border-solid border-[#F1F7ED] bg-[#222] p-4 text-base text-[#F1F7ED] disabled:bg-gray-500 disabled:text-black"
-            onClick={mintNFT}
-            disabled={loading || disable}
-          >
-            Mint
-          </button>
-        ) : (
-          <button
-            className="w-full rounded-lg border border-solid border-[#F1F7ED] bg-[#222] p-4 text-base text-[#F1F7ED]"
-            onClick={getURI}
-          >
-            Already minted! Show URI
-          </button>
-        )}
+        <h5 className="mb-2 text-3xl">ID: {tokenId}</h5>
+        <button
+          className="w-full rounded-lg border border-solid border-[#F1F7ED] bg-[#222] p-4 text-base text-[#F1F7ED]"
+          onClick={downloadImage}
+        >
+          Download image 😍
+        </button>
       </div>
     </div>
   );
