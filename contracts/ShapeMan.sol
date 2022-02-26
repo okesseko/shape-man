0a8deb13af18b8c4e8a2e56c0c243ad54a1c2984
okesseko <59650459+okesseko@users.noreply.github.com>
2022-02-27 00:08:09 +0800

    [update]: remove console log sol

--- contracts/ShapeMan.sol
+++ contracts/ShapeMan.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "hardhat/console.sol";
 
 contract ShapeMan is ERC721, ERC721Enumerable, Ownable {
 
